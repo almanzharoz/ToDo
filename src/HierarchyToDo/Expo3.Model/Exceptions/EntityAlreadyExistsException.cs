16e7e05b677463b00b4e67ad2851d6737f0df675
Михаил Пинчук <jojolkree@gmail.com>
2017-07-31 09:36:15 +0500

    Add search by name

---
+++ src/HierarchyToDo/Expo3.Model/Exceptions/EntityAlreadyExistsException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Expo3.Model.Exceptions
+{
+    public class EntityAlreadyExistsException : Exception
+    {
+        public EntityAlreadyExistsException()
+        {
+        }
+
+        public EntityAlreadyExistsException(string message) : base(message)
+        {
+        }
+
+        public EntityAlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
