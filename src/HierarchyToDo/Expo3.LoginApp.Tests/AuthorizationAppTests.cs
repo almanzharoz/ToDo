9b6adc7b6fa6a9c440cd4517ca548d07b4bc1458
Михаил Пинчук <jojolkree@gmail.com>
2017-08-01 16:02:07 +0500

    Login app tests init

---
+++ src/HierarchyToDo/Expo3.LoginApp.Tests/AuthorizationAppTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Expo3.LoginApp.Tests
+{
+    [TestClass]
+    public class AuthorizationAppTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
