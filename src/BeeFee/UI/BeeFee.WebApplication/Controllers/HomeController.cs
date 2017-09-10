bf9c975a232b75b59b13fff114a93a927e29ff41
pasha2100 <brat11@mail.ru>
2017-09-10 23:35:26 +0500

    more

--- src/BeeFee/UI/BeeFee.WebApplication/Controllers/HomeController.cs
+++ src/BeeFee/UI/BeeFee.WebApplication/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using BeeFee.ClientApp.Services;
+using BeeFee.Model.Projections;
+using BeeFee.Model.Services;
+using BeeFee.WebApplication.Models.Event;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BeeFee.WebApplication.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController<EventService>
     {
+        public HomeController(EventService service, CategoryService categoryService) : base(service, categoryService)
+        {
+        }
+
         public IActionResult Index()
         {
-            return RedirectToAction("Events", "Event");
-            return View();
+            //return RedirectToAction("Events", "Event");
+            var model = new EventFilterViewModel();
+            var nowTime = DateTime.UtcNow;
+            model.StartDate = new DateTime(nowTime.Year, nowTime.Month, 1);
+            model.EndDate = new DateTime(nowTime.Year, nowTime.Month + 1, 1).AddDays(-1);
+            model.Cities = _service.GetAllCities().ToList();
+            model.Categories = _categoryService.GetAllCategories<CategoryProjection>().Select(c => new SelectListItem() { Value = c.Id, Text = c.Name }).ToList();
+            return View(model);
         }
 
         public IActionResult About()
@@ -33,5 +50,6 @@ namespace BeeFee.WebApplication.Controllers
 	    {
 		    return View();
 	    }
+
     }
 }
